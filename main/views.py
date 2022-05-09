7b53bec8e7b45365d7113050af1051226742ea7e
SamRocan <SamClendenan@iCloud.com>
2022-05-09 09:49:06 +0100

    Added Basic search bar

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+#To view logs: docker logs vccf_web_1
 def index(request):
+    if(request.method == "POST"):
+        print(request.POST.get('search'))
     return render(request, 'main/index.html')
