e183373776758a25e5578c0b0a55e974818b72db
SamRocan <SamClendenan@iCloud.com>
2022-05-14 17:38:14 +0100

    Search is now sent to results page - Need to decipher whether search result is linked to a product, and if not redirect back to original page - Also start to analyze product searched for

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name="index"),
+    path('product/', views.product, name="product"),
+    path('product/<slug:productSlug>/', views.productHome, name="productHome")
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 #To view logs: docker logs vccf_web_1
+from django.urls import reverse
+
+
 def index(request):
-    if(request.method == "POST"):
-        print(request.POST.get('search'))
     return render(request, 'main/index.html')
+
+def product(request):
+    print(request.GET["productSearch"])
+    return redirect('productHome', productSlug=request.GET["productSearch"])
+
+def productHome(request, productSlug):
+    context = {
+        'product':productSlug
+    }
+    return render(request, 'main/productHome.html', context)
