324c1ccdcc5c12f2e79fb35918940444cee98a8a
SamRocan <SamClendenan@iCloud.com>
2022-07-25 06:22:43 +0100

    Template set up for scraping to be implemented. Also serves as a checkpoint before any major changes

--- main/scraper.py
+++ main/scraper.py
@@ -6,6 +6,7 @@ def searchResults(searchTerm):
     URL = 'https://www.ask.com/web?q='+searchTerm
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
+    results = [[],[]]
     for a in soup.find_all("a", {"class": "PartialSearchResults-item-title-link"}):
         if('searchResult' in a['data-analytics']):
             print(a.text)
