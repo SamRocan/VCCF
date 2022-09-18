2820b1540afaa5d716b9906c343a42189b00c3fc
SamRocan <SamClendenan@iCloud.com>
2022-09-18 17:32:52 +0100

    Setting up backup point before major change

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('product/', views.product, name="product"),
     path('product/<slug:productSlug>/', views.productHome, name="productHome"),
-    path('graphs/', ChartData.as_view(), name="ChartDate" )
+    path('graphs/', ChartData.as_view(), name="ChartDate")
 ]
