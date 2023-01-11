45bc6a427dca988b8687a2f5e6fa9e81de484d55
SamRocan <65802860+SamRocan@users.noreply.github.com>
2023-01-11 04:52:49 +0000

    set up basic user authentication

--- VCCF/urls.py
+++ VCCF/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),  # new
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
