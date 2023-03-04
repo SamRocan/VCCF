f4dd372562d020606ff36e6bfdfd2d126b294344
SamRocan <65802860+SamRocan@users.noreply.github.com>
2023-03-04 18:59:54 +0000

    Added password rules + initiated extra account views

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,12 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='user_login'),
-    #path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/confirm/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('profile/', views.profile, name='profile')
 ]
