fbdef1a22a8628628dc6f926f0fcdea7977697ba
SamRocan <65802860+SamRocan@users.noreply.github.com>
2023-03-04 18:49:09 +0000

    Stylized login and register views

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', views.user_login, name='user_login'),
+    #path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,21 +3,53 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import  render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, get_user_model
 from django.contrib import messages
 from main.models import Favourite
 
 def register(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:index")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request, template_name="registration/register.html", context={"register_form":form})
+    error_list = []
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect('index')
+        else:
+            users = get_user_model().objects.values_list('username', flat=True).distinct()
+            if request.POST.get('username', False) in users:
+                error_list.append("Username aleady taken")
+            emails = get_user_model().objects.values_list('email', flat=True).distinct()
+            if request.POST.get('email', False).lower() in emails:
+                error_list.append("email aleady taken")
+            if(request.POST.get('password1', False)) != request.POST.get('password2', False):
+                error_list.append("passwords must match")
+            else:
+                error_list.append("passwords does not meet requirement, please ....")
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    print(error_list)
+    return render (request, template_name="registration/register.html", context={"register_form":form, "error_list":error_list})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', False)
+        password = request.POST.get('password', False)
+        user = authenticate(request, username=username, password=password)
+        if (user is not None):
+            login(request, user)
+            return redirect('profile')
+        users = get_user_model().objects.values_list('username', flat=True).distinct()
+        if(username in users):
+            error_message=  'Username or password incorrect. Please try again'
+        else:
+            error_message = 'Account not found, please check your credentials'
+        context = {'error': error_message}    
+        return render (request, template_name="registration/login.html", context=context)
+    context = {'error':None}
+    return render (request, template_name="registration/login.html", context=context)
+
 
 def profile(request):
     favourites = Favourite.objects.filter(user=request.user)
